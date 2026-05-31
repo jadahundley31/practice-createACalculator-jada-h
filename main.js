644411abb524df6ba3f3c58603683b3868d7fd3f
Jada Hundley <jadahundley@gmail.com>
2026-05-30 19:59:53 -0500

    added more to the console.log.

--- main.js
+++ main.js
@@ -6,22 +6,21 @@
 function absoluteValueCalculation(num) {
     return Math.abs(num);
 }
-console.log(absoluteValueCalculation(-45.67));
-console.log(absoluteValueCalculation(-31));
+console.log("Absolute value of -45.67 and -31:", absoluteValueCalculation(-45.67), "and", absoluteValueCalculation(-31));
 
 //Power Calculation: Calculate and return the value of a base raised to a specific power.
 function powerCalculation(num1,num2) {
     return Math.pow(num1,num2);
 }
-console.log(powerCalculation(5,3));
-console.log(powerCalculation(2,2));
+console.log("5 raised to the power of 3:",powerCalculation(5,3));
+console.log("2 raised to the power of 2:", powerCalculation(2,2));
 
 //Square Root Finder: Calculate the square root of a number.
 function squareRootFinder(num) {
     return Math.sqrt(num);
 }
-console.log(squareRootFinder(144));
-console.log(squareRootFinder(16));
+console.log("Square root of 144:", squareRootFinder(144));
+console.log("Square root of 16:", squareRootFinder(16));
 
 //Maximum and Minimum Finder: From a given set of numbers, determine the largest and smallest values.
 let numArray = [3, 78, -12, 0.5, 27];
@@ -29,21 +28,22 @@ function findingMaximum() {
     function findingMinimum() {
         return Math.min(...numArray);
     }
-    console.log("Minimum: " + findingMinimum());
+    console.log("Minimum number: " + findingMinimum());
     return Math.max(...numArray);
 }
-console.log("Maximum: " + findingMaximum());
+console.log("Maximum number: " + findingMaximum());
 
 //Random Number Generator: Generate a random integer within a specified range.
-function randomNumGenerator() {
-    let num = Math.floor((Math.random() * 50) + 1);
+function randomNumGenerator(min, max) {
+    let num = Math.floor((Math.random() *  (max - min +1)) + min);
     return num;
 }
-console.log(randomNumGenerator());
+console.log("Random number between 1 and 50:", randomNumGenerator(1, 50));
 
 //Custom Rounding: Round a number to a specified number of decimal places.
 function customRounding() {
     let roundedNum = Math.round(23.67891 * 100) / 100;
     return roundedNum;
 }
-console.log(customRounding());
+console.log("Round 23.67891 to 2 decimal places:", customRounding());
+
